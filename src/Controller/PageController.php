92353a7e18c119ab1b88759d3721a910af005bb2
revicasamal <revicasamal4520@gmail.com>
2025-04-04 15:11:37 +0200

    ok

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -11,7 +11,7 @@ final class PageController extends AbstractController
     #[Route('/page', name: 'app_page')]
     public function index(): Response
     {
-        return $this->render('/index.html.twig', [
+        return $this->render('page/index.html.twig', [
             'controller_name' => 'PageController',
         ]);
     }
