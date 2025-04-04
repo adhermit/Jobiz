edcbdb91eca4099d0fd129abaf7c70c2f6f3de63
adhermit <maharjanraman456@gmail.com>
2025-04-04 16:09:08 +0200

    about page created

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -15,4 +15,11 @@ final class PageController extends AbstractController
             'controller_name' => 'PageController',
         ]);
     }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', [
+        ]);
+    }
 }
